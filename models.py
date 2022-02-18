f5649850f548b48649d44ab50999b2ffe843c292
Michael Jones <michaeljones@Michaels-MacBook-Pro-2.local>
2022-02-18 08:43:34 +0000

    Addition of the requirements.txt file

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from ast import In
+gifrom ast import In
 from sqlalchemy import create_engine, Column, String, Integer, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
